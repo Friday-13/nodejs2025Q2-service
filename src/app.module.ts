9cb28efcb14d0100001ba4fe02203055d1ea5fd3
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-06-01 23:55:59 +0300

    feat: implement PORT value is stored into .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,12 @@ import { TracksModule } from './tracks/tracks.module';
 import { ArtistsModule } from './artists/artists.module';
 import { AlbumsModule } from './albums/albums.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { ConfigModule } from '@nestjs/config';
+import configs from './configs';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ load: [configs], isGlobal: true }),
     UsersModule,
     TracksModule,
     ArtistsModule,
