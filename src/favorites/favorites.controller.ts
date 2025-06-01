aded4da1804414eeb44b496d51a3550cc2da649b
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-06-01 03:09:59 +0300

    refactor: rewrite favorites controller

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -23,78 +23,82 @@ export class FavoritesController {
     return await this.favoritesService.findAll();
   }
 
-  @Post('track/:id')
-  async addTrack(@Param('id', ParseUUIDPipe) id: string) {
+  private async addEntity<T>(
+    id: string,
+    addCallback: (id: string) => Promise<T>,
+  ) {
     try {
-      return await this.favoritesService.addTrack(id);
+      return await addCallback(id);
     } catch (err) {
       if (err instanceof RecordDoesntExist) {
         throw new UnprocessableEntityException(err.message);
       }
       throw err;
     }
+  }
+
+  private async deleteEntity<T>(
+    id: string,
+    deleteCallback: (id: string) => Promise<T>,
+  ) {
+    try {
+      await deleteCallback(id);
+    } catch (err) {
+      if (err instanceof RecordDoesntExist) {
+        throw new NotFoundException(err.message);
+      }
+      throw err;
+    }
+  }
+
+  @Post('track/:id')
+  async addTrack(@Param('id', ParseUUIDPipe) id: string) {
+    return await this.addEntity(
+      id,
+      this.favoritesService.addTrack.bind(this.favoritesService),
+    );
   }
 
   @Delete('track/:id')
   @HttpCode(204)
   async deleteTrack(@Param('id', ParseUUIDPipe) id: string) {
-    try {
-      await this.favoritesService.deleteTrack(id);
-    } catch (err) {
-      if (err instanceof RecordDoesntExist) {
-        throw new NotFoundException(err.message);
-      }
-      throw err;
-    }
+    return await this.deleteEntity(
+      id,
+      this.favoritesService.deleteTrack.bind(this.favoritesService),
+    );
   }
 
   @Post('artist/:id')
   async addArtist(@Param('id', ParseUUIDPipe) id: string) {
-    try {
-      return await this.favoritesService.addArtist(id);
-    } catch (err) {
-      if (err instanceof RecordDoesntExist) {
-        throw new UnprocessableEntityException(err.message);
-      }
-      throw err;
-    }
+    return await this.addEntity(
+      id,
+      this.favoritesService.addArtist.bind(this.favoritesService),
+    );
   }
 
   @Delete('artist/:id')
   @HttpCode(204)
   async deleteArtist(@Param('id', ParseUUIDPipe) id: string) {
-    try {
-      await this.favoritesService.deleteArtist(id);
-    } catch (err) {
-      if (err instanceof RecordDoesntExist) {
-        throw new NotFoundException(err.message);
-      }
-      throw err;
-    }
+    return await this.deleteEntity(
+      id,
+      this.favoritesService.deleteArtist.bind(this.favoritesService),
+    );
   }
 
   @Post('album/:id')
   async addAlbum(@Param('id', ParseUUIDPipe) id: string) {
-    try {
-      return await this.favoritesService.addAlbum(id);
-    } catch (err) {
-      if (err instanceof RecordDoesntExist) {
-        throw new UnprocessableEntityException(err.message);
-      }
-      throw err;
-    }
+    return await this.addEntity(
+      id,
+      this.favoritesService.addAlbum.bind(this.favoritesService),
+    );
   }
 
   @Delete('album/:id')
   @HttpCode(204)
   async deleteAlbum(@Param('id', ParseUUIDPipe) id: string) {
-    try {
-      await this.favoritesService.deleteAlbum(id);
-    } catch (err) {
-      if (err instanceof RecordDoesntExist) {
-        throw new NotFoundException(err.message);
-      }
-      throw err;
-    }
+    return await this.deleteEntity(
+      id,
+      this.favoritesService.deleteAlbum.bind(this.favoritesService),
+    );
   }
 }
