cabbc7b200e2a698df4c6e4ce971294b31c660df
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-06-02 16:03:26 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,30 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Home Library Service')
+    .addServer('/')
+    .setDescription('Home music library service')
+    .setVersion('1.0')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    deepScanRoutes: true,
+  };
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, documentFactory);
 
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
